e95705b65d65579eb3adeb66ad669d6bd977a7b2
Melody <64640893+MyMelodyUwU@users.noreply.github.com>
2022-03-02 10:53:06 +1100

    init

---
+++ stuff.R
@@ -0,0 +1,23 @@
+library("tidyverse")
+library("devtools")
+library("glue")
+library("dplyr")
+library("stringr")
+library("ggplot2")
+library("knitr")
+library("ggrepel")
+library("patchwork")
+library("janitor")
+library("usethis")
+library("roxygen2")
+library("testthat")
+
+banana = read.csv("data.csv", header=TRUE)
+
+attach(banana)
+
+summary(banana$val)
+
+sd(banana$val)
+
+
