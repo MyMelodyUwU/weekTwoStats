36ebbc69d98f846f8a085fe4f1a85f9e55786748
Melody <64640893+MyMelodyUwU@users.noreply.github.com>
2022-03-04 11:21:28 +1100

    task 5, task 2 unknown

---
+++ workshop.R
@@ -0,0 +1,33 @@
+# Task 2
+
+q1 <- 2^10
+q2 <- log(10, base = 2)
+q3a <- cospi(2)
+q3b <- asin(0.5)
+q3c <- dunno
+
+# Task 3
+
+#X <- 2 invalid
+X1 <- 3
+X_ <- 4
+#_X <- 5 invalid
+.X <- 6
+X. <- 7
+
+#Task 4
+
+temp_C <- c(23.5,23.4,22.1,19.5)
+sd(temp_C)
+i <- 1
+tempF <-  c(1,2,3,4)
+for(i in temp_C) {
+  tempF <- temp_C * 33.8
+  print(i)
+}
+tempF
+
+# Task 5
+
+
+
